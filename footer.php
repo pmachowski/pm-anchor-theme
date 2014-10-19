940ca56907b2dba6ad4d18b4c1111047b0078609
Piotr Machowski <piotr@machowski.co>
2014-10-19 13:53:11 +0200

    add about section

--- footer.php
+++ footer.php
@@ -1,4 +1,22 @@
-		<div class="wrap">
+
+			<div class="fullblue">
+				<div class="wrap about">
+					<section>
+						<h1>About Me</h1>
+						<p>Hi, my name is Piotr Machowski. I work as a senior JavaScript developer specializing in a massive AngularJS applications. International conferences speaker, design lover, former mobile developer.</p>
+					</section>
+					<section>
+						<h1>Follow Me</h1>
+						<div class="tw-outside">
+							<a href="<?php echo twitter_url(); ?>" class="tw-link">
+								<span class="fa fa-twitter"></span>
+							</a>
+						</div>
+					</section>
+				</div>
+			</div>
+
+			<div class="wrap">
 	            <footer id="bottom">
 	                <small>&copy; <?php echo date('Y'); ?> <?php echo site_name(); ?>. All rights reserved.</small>
 
